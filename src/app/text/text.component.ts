6f05eb438688138d11cf3e396cbfe1e77ab939e8
wendyrs <wreyes@phantasia.pe>
2017-12-04 17:38:20 -0500

    adding strings

--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -3,11 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
-  styleUrls: ['./text.component.css']
+  styleUrls: ['./text.component.css'],
 })
 export class TextComponent implements OnInit {
 
-  strings = ["Saab", "Volvo", "BMW", "Saab2", "Volvo2", "BMW2"];
+  strings = ["¡Quiero torta!",
+             "La niña baila marinera en el patio",
+             "Pensando en lo mismo",
+             "Me gusta la música",
+             "Camaron que se duerme se lo lleva la corriente",
+             "Laboratoria es código que transforma",
+             "Todo va a estar bien",
+             "Me encanta bailar para relajarme",
+             "El texto se acabó. Fin."];
 
   constructor() { }
 
