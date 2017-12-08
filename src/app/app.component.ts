7312c49ca56428544f21076dcb57684248201c4c
flordeciruela <wendy.reyessan@gmail.com>
2017-12-07 22:01:41 -0500

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  counter: number;
   title = 'Cadáver exquisito';
+  form: FormGroup;
+
+  private static CustomValidator(control: AbstractControl) {
+    return control.value ? null: {angularFundamentals: false}
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.counter = 1;
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group(controlsConfig: {
+      k1: ['', [
+        Validators.minLength( minLength: 7), //home
+        Validators.maxLength( maxLength: 9), //mobile
+        AppComponent.CustomValidator
+      ]]
+    });
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  onSubmit(event) {
+    if (this.form.valid) {
+      console.log('valid', this.form.valid);
+      console.log('k1', this.form.get('k1').value);
+    }
+  }
 }
